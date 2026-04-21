ba4e44cc343db4b5a79aba52f457b5f81e2a9f9a
Muhammad Kashif <154686104+Muhammadkashif20@users.noreply.github.com>
2026-04-21 22:41:04 +0500

    carousel done

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,7 +4,8 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <header className="fixed top-0 left-0 w-full z-50 bg-black/40 backdrop-blur-md border-b border-white/10">
+    <div className="mt-8">
+    <header className="fixed top-0 left-0 w-full z-50 bg-black/40 backdrop-blur-md border-b border-white/10 ">
       
       <div className="max-w-7xl mx-auto flex items-center justify-between px-6 py-4">
         
@@ -38,9 +39,9 @@ const Header = () => {
         <button className="bg-orange-500 hover:bg-orange-600 text-white px-5 py-2 rounded-full text-sm font-semibold transition shadow-lg">
           Order Now
         </button>
-
       </div>
     </header>
+    </div>
   );
 };
 
--- src/Pages/Home/BannerCarousel.jsx
+++ src/Pages/Home/BannerCarousel.jsx
@@ -1,40 +1,31 @@
-import { ChevronLeft, ChevronRight } from "@untitledui/icons";
-import { Carousel } from "../../Components/application/carousel/carousel-base";
+import React from 'react';
+import { Carousel } from 'antd';
+import banner1 from '../../assets/images/banner1.jpg';
+import banner2 from '../../assets/images/banner2.jpg';
+import banner3 from '../../assets/images/banner3.jpg';
+// const contentStyle = {
+//   margin: 0,
+//   height: '160px',
+//   color: '#fff',
+//   lineHeight: '160px',
+//   textAlign: 'center',
+//   background: '#364d79',
+// };
+const BannerCarousel = () => (
+  <Carousel autoplay>
+    <div>
+      <img src={banner1} className="w-full h-[350px] object-cover"
+      />
+    </div>
+    <div>
+      <img src={banner2} className="w-full h-[350px] object-cover"
+      />
+    </div>
+    <div>
+      <img src={banner3} className="w-full h-[350px] object-cover"
+      />
+    </div>
 
- const BannerCarousel = () => {
-    return (
-        <Carousel.Root className="relative aspect-[1.6] max-w-160">
-            <Carousel.PrevTrigger className="absolute top-1/2 left-5 z-10 flex size-11 -translate-y-1/2 cursor-pointer items-center justify-center rounded-full bg-alpha-white/90 p-2 text-fg-secondary outline-focus-ring backdrop-blur-xs focus-visible:outline-2 focus-visible:outline-offset-2 disabled:cursor-not-allowed disabled:opacity-50">
-                <ChevronLeft className="size-6" />
-            </Carousel.PrevTrigger>
-            <Carousel.NextTrigger className="absolute top-1/2 right-5 z-10 flex size-11 -translate-y-1/2 cursor-pointer items-center justify-center rounded-full bg-alpha-white/90 p-2 text-fg-secondary outline-focus-ring backdrop-blur-xs focus-visible:outline-2 focus-visible:outline-offset-2 disabled:cursor-not-allowed disabled:opacity-50">
-                <ChevronRight className="size-6" />
-            </Carousel.NextTrigger>
-
-            <div className="absolute bottom-4 left-1/2 z-10 -translate-x-1/2">
-                <CarouselIndicator framed size="lg" />
-            </div>
-
-            <Carousel.Content className="gap-2">
-                <Carousel.Item className="overflow-hidden rounded-xl">
-                    <img alt="Image by Unsplash" src="/src/assets/img/banner1.jpg" className="size-full object-cover" />
-                </Carousel.Item>
-                <Carousel.Item className="overflow-hidden rounded-xl">
-                    <img
-                        alt="Image by Unsplash"
-                        src="/src/assets/img/banner2.jpg"
-                        className="size-full object-cover"
-                    />
-                </Carousel.Item>
-                <Carousel.Item className="overflow-hidden rounded-xl">
-                    <img
-                        alt="Image by Unsplash"
-                        src="src/assets/img/banner3.jpg"
-                        className="size-full object-cover"
-                    />
-                </Carousel.Item>
-            </Carousel.Content>
-        </Carousel.Root>
-    );
-};
-export default BannerCarousel;
+  </Carousel>
+);
+export default BannerCarousel; 
