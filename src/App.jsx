533291f772fbe6ff791b15b13a79f306343552bc
Muhammad Kashif <154686104+Muhammadkashif20@users.noreply.github.com>
2026-04-23 20:13:48 +0500

    routing done

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './Pages/Home/Home'
+import Menu from './Pages/Menu/Menu'
+import Contact from './Pages/Contact/Contact'
+import Deals from './Pages/Deals/Deals'
+import About from './Pages/About/About'
 
 const App = () => {
   return (
-<>
-<BrowserRouter>
-<Routes>
-  <Route path="/" element={<Home />} />
-</Routes>
-</BrowserRouter>
-</>  
-)
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path='/menu' element={<Menu />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/deals' element={<Deals />} />
+          <Route path='/about' element={<About />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
 }
 
 export default App
